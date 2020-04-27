870bae307541c4768c7ddcc4b6ada1b9bf48db89
Dev-Rikshith <bussarikshith@gmail.com>
2020-04-28 02:25:57 +0530

    Writes the flash cards to a file and saves it on the local machine

--- src/com/flashcardbuilder/java/FlashCardBuilder.java
+++ src/com/flashcardbuilder/java/FlashCardBuilder.java
@@ -4,7 +4,11 @@ import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import javax.swing.*;
 
@@ -51,7 +55,6 @@ public class FlashCardBuilder {
 		JLabel qJLabel = new JLabel("Question");
 		JLabel aJLabel = new JLabel("Answer");
 		
-		
 		//Initialize the menu bar and its items
 		JMenuBar menuBar = new JMenuBar();
 		JMenu menu = new JMenu("File");
@@ -62,9 +65,6 @@ public class FlashCardBuilder {
 		menu.add(newMenuItem);
 		menu.add(saveMenuItem);
 		menuBar.add(menu);
-		
-		
-		
 		
 		//Add Components to the pane
 		mainPane.add(qJLabel);
@@ -84,20 +84,18 @@ public class FlashCardBuilder {
 		newMenuItem.addActionListener(new newMenuItemListener());
 		saveMenuItem.addActionListener(new saveMenuItemListener());
 		
-		
-		
-		
-		
 	}
 	
 	public class nextButtonListener implements ActionListener{
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			System.out.println("Next Button Clicked");
+			//Data saved to ArrayList and cleared
+			FlashCards card = new FlashCards(question.getText(), answer.getText());
+			myCards.add(card);
+			clearCard();
 			
 		}
-		
 	}
 	
 	
@@ -106,7 +104,6 @@ public class FlashCardBuilder {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			System.out.println("New Button Clicked");
-			
 		}
 		
 	}
@@ -115,14 +112,54 @@ public class FlashCardBuilder {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			System.out.println("Save Button Clicked");
+			//Add the card to ArrayList
+			FlashCards card = new FlashCards(question.getText(), answer.getText());
+			myCards.add(card);
+			
+			//Choose filename and write the ArrayList to the file
+			JFileChooser fileSave = new JFileChooser();
+			fileSave.showSaveDialog(frame);
+			saveFile(fileSave.getSelectedFile());
 			
 		}
 		
 	}
 	
+	private void clearCard() {
+		//function clears text and focus on the question
+		question.setText("");
+		answer.setText("");
+		question.requestFocus();
+	}
+	
+	private void saveFile(File selectedFile) {
+		//The file writing is error prone so we use try-catch block
+		try {
+			//Initialize a writer
+			BufferedWriter writer = new BufferedWriter(new FileWriter(selectedFile));
+			//Initialize a Iterator and write to the above selected file
+			Iterator<FlashCards> cardIterator = myCards.iterator();
+			while(cardIterator.hasNext()) {
+				FlashCards card = (FlashCards)cardIterator.next();
+				writer.write(card.getQuestion() + "/");
+				writer.write(card.getAnswer() + "\n");
+				
+			}
+			writer.close();
+			
+			
+		} catch (Exception e) {
+			//Print the error message and the StackTrace in case of any exception occurence 
+			System.out.println("Could not write to file");
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
 
 	public static void main(String[] args) {
+		//invokeLater method updates or performs any task on Event dispatcher thread asynchronously.
 		SwingUtilities.invokeLater(new Runnable() {
 			
 			@Override
